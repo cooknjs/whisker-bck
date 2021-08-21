d7a47dfa2574f6342b7d48ea03d1134751fc5292
spadino <spada.andrea@gmail.com>
2021-08-21 19:49:25 -0300

    lint

--- src/commands/serve.js
+++ src/commands/serve.js
@@ -1,8 +1,8 @@
-const {Command, flags} = require('@oclif/command')
+const { Command, flags } = require('@oclif/command')
 
 class ServeCommand extends Command {
-  async run() {
-    const {flags} = this.parse(ServeCommand)
+  async run () {
+    const { flags } = this.parse(ServeCommand)
     const name = flags.name || 'world'
     this.log(`hello ${name} from /data/projects/cookn/components/whisker/src/commands/serve.js`)
   }
@@ -14,7 +14,7 @@ Extra documentation goes here
 `
 
 ServeCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
+  name: flags.string({ char: 'n', description: 'name to print' })
 }
 
 module.exports = ServeCommand
